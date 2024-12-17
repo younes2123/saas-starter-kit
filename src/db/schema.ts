251cbed16238ba11f3050d3a38a2b485ca4a512e
younes2123 <younestofo@gmail.com>
2024-12-17 19:22:40 +0100

    Remove Group related functionality and keep only core app tables

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -10,7 +10,7 @@ import {
   timestamp,
 } from "drizzle-orm/pg-core";
 
-export const roleEnum = pgEnum("role", ["member", "admin"]);
+export const roleEnum = pgEnum("role", ["member", "owner"]);
 export const accountTypeEnum = pgEnum("type", ["email", "google", "github"]);
 
 export const users = pgTable("gf_user", {
@@ -134,25 +134,6 @@ export const subscriptions = pgTable(
   })
 );
 
-export const following = pgTable(
-  "gf_following",
-  {
-    id: serial("id").primaryKey(),
-    userId: serial("userId")
-      .notNull()
-      .references(() => users.id, { onDelete: "cascade" }),
-    foreignUserId: serial("foreignUserId")
-      .notNull()
-      .references(() => users.id, { onDelete: "cascade" }),
-  },
-  (table) => ({
-    userIdForeignUserIdIdx: index("following_user_id_foreign_user_id_idx").on(
-      table.userId,
-      table.foreignUserId
-    ),
-  })
-);
-
 /**
  * newsletters - although the emails for the newsletter are tracked in Resend, it's beneficial to also track
  * sign ups in your own database in case you decide to move to another email provider.
@@ -164,122 +145,16 @@ export const newsletters = pgTable("gf_newsletter", {
   email: text("email").notNull().unique(),
 });
 
-export const groups = pgTable(
-  "gf_group",
-  {
-    id: serial("id").primaryKey(),
-    userId: serial("userId")
-      .notNull()
-      .references(() => users.id, { onDelete: "cascade" }),
-    name: text("name").notNull(),
-    description: text("description").notNull(),
-    isPublic: boolean("isPublic").notNull().default(false),
-    bannerId: text("bannerId"),
-    info: text("info").default(""),
-    youtubeLink: text("youtubeLink").default(""),
-    discordLink: text("discordLink").default(""),
-    githubLink: text("githubLink").default(""),
-    xLink: text("xLink").default(""),
-  },
-  (table) => ({
-    userIdIsPublicIdx: index("groups_user_id_is_public_idx").on(
-      table.userId,
-      table.isPublic
-    ),
-  })
-);
-
-export const memberships = pgTable(
-  "gf_membership",
-  {
-    id: serial("id").primaryKey(),
-    userId: serial("userId")
-      .notNull()
-      .references(() => users.id, { onDelete: "cascade" }),
-    groupId: serial("groupId")
-      .notNull()
-      .references(() => groups.id, { onDelete: "cascade" }),
-    role: roleEnum("role").default("member"),
-  },
-  (table) => ({
-    userIdGroupIdIdx: index("memberships_user_id_group_id_idx").on(
-      table.userId,
-      table.groupId
-    ),
-  })
-);
-
-export const invites = pgTable("gf_invites", {
-  id: serial("id").primaryKey(),
-  token: text("token")
-    .notNull()
-    .default(sql`gen_random_uuid()`)
-    .unique(),
-  groupId: serial("groupId")
-    .notNull()
-    .references(() => groups.id, { onDelete: "cascade" }),
-  tokenExpiresAt: timestamp("tokenExpiresAt", { mode: "date" }).notNull(),
-});
-
-export const events = pgTable("gf_events", {
-  id: serial("id").primaryKey(),
-  groupId: serial("groupId")
-    .notNull()
-    .references(() => groups.id, { onDelete: "cascade" }),
-  name: text("name").notNull(),
-  description: text("description").notNull(),
-  imageId: text("imageId"),
-  startsOn: timestamp("startsOn", { mode: "date" }).notNull(),
-});
-
-export const notifications = pgTable("gf_notifications", {
-  id: serial("id").primaryKey(),
-  userId: serial("userId")
-    .notNull()
-    .references(() => users.id, { onDelete: "cascade" }),
-  groupId: serial("groupId")
-    .notNull()
-    .references(() => groups.id, { onDelete: "cascade" }),
-  postId: integer("postId"),
-  isRead: boolean("isRead").notNull().default(false),
-  type: text("type").notNull(),
-  message: text("message").notNull(),
-  createdOn: timestamp("createdOn", { mode: "date" }).notNull(),
-});
-
-export const posts = pgTable("gf_posts", {
-  id: serial("id").primaryKey(),
-  userId: serial("userId")
-    .notNull()
-    .references(() => users.id, { onDelete: "cascade" }),
-  groupId: serial("groupId")
-    .notNull()
-    .references(() => groups.id, { onDelete: "cascade" }),
-  title: text("title").notNull(),
-  message: text("message").notNull(),
-  createdOn: timestamp("createdOn", { mode: "date" }).notNull(),
-});
-
-export const reply = pgTable(
-  "gf_replies",
-  {
-    id: serial("id").primaryKey(),
-    userId: serial("userId")
-      .notNull()
-      .references(() => users.id, { onDelete: "cascade" }),
-    postId: serial("postId")
-      .notNull()
-      .references(() => posts.id, { onDelete: "cascade" }),
-    groupId: serial("groupId")
-      .notNull()
-      .references(() => groups.id, { onDelete: "cascade" }),
-    message: text("message").notNull(),
-    createdOn: timestamp("createdOn", { mode: "date" }).notNull(),
-  },
-  (table) => ({
-    postIdIdx: index("replies_post_id_idx").on(table.postId),
-  })
-);
+// export const notifications = pgTable("gf_notifications", {
+//   id: serial("id").primaryKey(),
+//   userId: serial("userId")
+//     .notNull()
+//     .references(() => users.id, { onDelete: "cascade" }),
+//   isRead: boolean("isRead").notNull().default(false),
+//   type: text("type").notNull(),
+//   message: text("message").notNull(),
+//   createdOn: timestamp("createdOn", { mode: "date" }).notNull(),
+// });
 
 /**
  * RELATIONSHIPS
@@ -288,37 +163,6 @@ export const reply = pgTable(
  * in your code.
  */
 
-export const groupRelations = relations(groups, ({ many }) => ({
-  memberships: many(memberships),
-}));
-
-export const membershipRelations = relations(memberships, ({ one }) => ({
-  user: one(users, { fields: [memberships.userId], references: [users.id] }),
-  profile: one(profiles, {
-    fields: [memberships.userId],
-    references: [profiles.userId],
-  }),
-  group: one(groups, {
-    fields: [memberships.groupId],
-    references: [groups.id],
-  }),
-}));
-
-export const postsRelationships = relations(posts, ({ one }) => ({
-  user: one(users, { fields: [posts.userId], references: [users.id] }),
-  group: one(groups, { fields: [posts.groupId], references: [groups.id] }),
-}));
-
-export const followingRelationship = relations(following, ({ one }) => ({
-  foreignProfile: one(profiles, {
-    fields: [following.foreignUserId],
-    references: [profiles.userId],
-  }),
-  userProfile: one(profiles, {
-    fields: [following.userId],
-    references: [profiles.userId],
-  }),
-}));
 
 /**
  * TYPES
@@ -328,26 +172,10 @@ export const followingRelationship = relations(following, ({ one }) => ({
  * in a component or function.
  */
 export type Subscription = typeof subscriptions.$inferSelect;
-export type Group = typeof groups.$inferSelect;
-export type NewGroup = typeof groups.$inferInsert;
-export type Membership = typeof memberships.$inferSelect;
-
-export type Event = typeof events.$inferSelect;
-export type NewEvent = typeof events.$inferInsert;
 
 export type User = typeof users.$inferSelect;
 export type Profile = typeof profiles.$inferSelect;
 
-export type Notification = typeof notifications.$inferSelect;
-
-export type Post = typeof posts.$inferSelect;
-export type NewPost = typeof posts.$inferInsert;
-
-export type Reply = typeof reply.$inferSelect;
-export type NewReply = typeof reply.$inferInsert;
-
-export type Following = typeof following.$inferSelect;
-
-export type GroupId = Group["id"];
+// export type Notification = typeof notifications.$inferSelect;
 
 export type Session = typeof sessions.$inferSelect;
